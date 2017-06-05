d676e8785f669f4a335c277ac9559d60685b32d9
Andrew Jones <jones.gaffney1920@gmail.com>
2017-06-05 16:07:11 -0400

    First Commit

---
+++ src/testingGrounds/JarTest.java
@@ -0,0 +1,41 @@
+package testingGrounds;
+
+import utilityForHire.UtilityDate;
+import utilityForHire.UtilityNumberFormat;
+
+public class JarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		System.out.println(UtilityDate.displayCalendar(11, 2010));
+		System.out.println(UtilityDate.getLastDayOfMonth(2, 1996));
+		System.out.println(UtilityDate.isLeapYear(1992));
+		System.out.println(UtilityNumberFormat.toCurrency(19.9752));
+		System.out.println(UtilityNumberFormat.toDecimal(3.4858732145831458, 5));
+		System.out.println(UtilityNumberFormat.toPercent(.0075));
+		System.out.println(UtilityNumberFormat.maxOf(99, 45));
+		System.out.println(UtilityNumberFormat.minOf(99, 45));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
+		
+	}
+
+}
