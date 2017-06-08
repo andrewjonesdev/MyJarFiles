bc999bc7ee8cf62227c3a862da75ab724772294f
Andrew Jones <jones.gaffney1920@gmail.com>
2017-06-08 16:59:37 -0400

    Updated with Longs and Floats also updated sentence

--- src/testingGrounds/JarTest.java
+++ src/testingGrounds/JarTest.java
@@ -1,13 +1,16 @@
 package testingGrounds;
 
+import java.util.Scanner;
+
 import utilityForHire.UtilityDate;
 import utilityForHire.UtilityNumberFormat;
+import utilityForHire.UtilityStringNScan;
 
 public class JarTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated constructor stub
-		System.out.println(UtilityDate.displayCalendar(11, 2010));
+		/*System.out.println(UtilityDate.displayCalendar(11, 2010));
 		System.out.println(UtilityDate.getLastDayOfMonth(2, 1996));
 		System.out.println(UtilityDate.isLeapYear(1992));
 		System.out.println(UtilityDate.toString(2, 29, 1996, "/"));
@@ -35,8 +38,20 @@ public class JarTest {
 		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
 		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
 		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
-		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));
-		
-	}
-
+		System.out.println(UtilityNumberFormat.randomOf(9912, 9917));*/
+		/*int j = 12;
+		boolean keepLooping = false;
+		Scanner numScan = new Scanner(System.in);
+	/*numParamater("I'm sorry you didn't press a valid number. Please try agqain", UtilityStringNScan.intCheck("Welcome to the ATM\nPress 1 to Deposit Cash\nPress 2 to Withdraw Cash\nPress 3 to Check your Balance\nPress 4 for a Receipt", keepLooping, numScan), UtilityStringNScan.intCheck("Welcome to the ATM\nPress 1 to Deposit Cash\nPress 2 to Withdraw Cash\nPress 3 to Check your Balance\nPress 4 for a Receipt", keepLooping, numScan)<4);
+	*/}
+	/*public static void numParamater(String invalid, int selection, boolean test){
+	
+		do{
+			UtilityStringNScan.intCheck(String question1 boolean keepLooping, Scanner numScan)
+		if(test){
+			 System.out.println(invalid);
+			 selection = selection;
+		}
+	}while(test);
+	}*/
 }
